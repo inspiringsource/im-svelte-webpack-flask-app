55a0552984df594507f83701954c32f8db72be66
inspiringsource <ab.b2k3@gmail.com>
2023-05-24 17:07:00 +0200

    frontend more accurate naming convention

--- frontend/src/OrderList.svelte
+++ frontend/src/OrderList.svelte
@@ -14,7 +14,7 @@
     {#each orders as order (order.id)}
       <li>
         <h2>Order #{order.id}</h2>
-        <p>Product ID: {order.productId}</p>
+        <p>SKU: {order.productId}</p> <!-- Stock Keeping Unit is the product ID -->
         <p>Quantity: {order.quantity}</p>
       </li>
     {/each}
